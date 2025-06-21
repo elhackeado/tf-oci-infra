2ee1b47c00a0996096ab33a6697de5dcf6662209
aman <theelhackeado@gmail.com>
2025-06-21 11:40:15 +0530

    destroying infra

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,12 @@
-output "ssh_private_key" {
-  value = module.k8s.ssh_private_key
-  sensitive = true
-}
+# output "ssh_private_key" {
+#   value = module.k8s.ssh_private_key
+#   sensitive = true
+# }
 
-output "ssh_public_key" {
-  value = module.k8s.ssh_public_key
-}
+# output "ssh_public_key" {
+#   value = module.k8s.ssh_public_key
+# }
 
-output "nlb_public_ip" {
-  value = module.k8s.nlb_public_ip
-}
+# output "nlb_public_ip" {
+#   value = module.k8s.nlb_public_ip
+# }
